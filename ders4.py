051fbbb81502addfd073ba5e320d2a3272a6521a
Nasibov1995 <nasib_az@mail.ru>
2023-08-17 15:35:02 +0400

    Fayllar Yuklendi

---
+++ ders4.py
@@ -0,0 +1,69 @@
+# x = ['Fuad','Tural','Samir']
+# x = [1,2,3,4,5]
+# print(x[0])
+# x.pop()
+# print(x)
+# x.remove('Samir')
+# print(x)
+# print(x.remove('Fuad'))
+
+# x.append('Hesen')
+# print(x)
+
+# x.insert(2,'Hesen')
+# print(x)
+
+# print(len(x))
+
+# print(sum(x))
+
+# x.reverse()
+
+# print(x)
+
+# x = [ [1,2,3] , [4,5,6] , [7,8,9] ]
+
+# print(x[1][1])
+
+# a = [1,'Fuad',True,2.5]
+
+# print(a[1])
+
+# x = list(('Fuad','Aslan'))
+
+# print(x)
+
+# x = []
+
+
+# x = ['Fuad','Tural','Samir']
+
+# a = x
+
+# print(a)
+
+# x = ['Fuad','Tural','Samir','Mehemmed']
+
+# for i in x:
+#     if 'a' in i:
+#         print(i)
+       
+    
+# meyveler = ["alma", "banan", "gilas", "kivi", "manqo"]
+
+# print(type(meyveler))
+
+# newlist = []
+
+# for x in meyveler:
+#   if "a" in x:
+#     newlist.append(x)
+
+# print(newlist)
+
+# x = ['Fuad','Tural','Samir']
+
+# x[1] = 'Hesen'
+
+# print(x)
+
