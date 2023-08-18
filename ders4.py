722c7b6f444c74e835449759c62d55b005240c3e
Nasibov1995 <nasib_az@mail.ru>
2023-08-18 16:49:47 +0400

    Fayllar yuklendi

--- ders4.py
+++ ders4.py
@@ -68,3 +68,191 @@
 # print(x)
 # 
 
+# a = { 'Baki': {'Unvan':'Baki'},'ad':[['Hesen', 'Fuad']]}
+# for i in a.items():
+#     print(i)
+
+# a = {'child1':'Emil','yash':15}
+# b = {'child2':'Ayxan','yash':10}
+
+# c = {'member1':a,'member2':b}
+
+# print(c['member1'])
+
+# a = {5,6,7,8,9}
+
+# a.pop()
+# print(a)
+
+# a = [1,2,3,4]
+
+# print(a[0])
+# print('')
+# print(a[-1])
+
+# test_list = [5, 6, [], 3, [], [], 9]
+
+# for i in test_list:
+#     if i != []:
+#         print(i)
+    
+# l = ['Fuad', 'Ayxan']
+# secim = int(input(' Secim edin :\n (1) Adi daxil edin \n (2) Adi silin '))
+
+
+# if secim == 1:
+#     ad = input('Adi secin : ')
+#     if len(ad) < 15:
+#         l.append(ad)
+#         print(l)
+#         for i in l:
+#             print(i)
+#     else:
+#         print('Maksimum 15 characters')
+        
+    
+# if secim == 2:
+#     ad = input('Adi secin : ')
+#     l.remove(ad)
+#     print(l)
+#     for i in l:
+#         print(i)
+    
+# x = int(input('Eded daxil edin : '))
+
+# if x < 0:
+#     print('Bu eded menfidir')
+# else:
+#     print('Bu eded musbetdir')
+
+# a =(1,2,3,4)
+
+# print(a.index(3))
+
+
+# a = {'Sheher' :'Baki','Unvan' :'Serifzade'}
+
+# ad = input('daxil edin (1) Key - (2)Values: ')
+
+
+# if ad == '':
+#     print('hecne Daxil etmediniz')
+# if ad == '1':
+#     print(a.keys())
+# if ad == '2':
+#     print(a.values())
+
+
+# a = ['Fuad','Aslan','Afaq']
+    
+# ad = int(input('Ad daxil edin (1) Adi silin (2) : '))
+
+# if ad == 1:
+#     s = input('Ad daxil edin : ')
+#     a.append(s)
+#     print(a)
+#     for i in a:
+#         print(i)
+# if ad == 2:
+#     s = input('Adi silin : ')
+#     a.remove(s)
+#     print(a)
+#     for i in a:
+#         print(i)
+
+# a = [1,2,3,4,5]
+
+# print(a[0])
+# print()
+# print(a[-1])
+
+
+# test_list = [5, 6, [], 3, [], [], 9]
+
+
+# for x in test_list:
+#     if x != []:
+#         print(x)
+
+# a = int(input('Eded daxil edin : '))
+
+# if a < 0 :
+#     print('Bu eded menfidir ')
+# else:
+#     print('Bu eded musbetdir ')
+    
+
+# a = {'Sheher':'Baki','Unvan':[['Serifzade','Abbasov']]}
+
+
+# print(a.get('Unvan'))
+
+
+# a.pop('Unvan')
+# print(a)
+
+# a.popitem()
+
+# print(a)
+
+# a.update({'Sheher':'Sumqayit'})
+
+# print(a)
+
+# a.clear()
+
+# print(a)
+
+
+# a = {'ad' : ' Emil'}
+
+# b = {'ad' : 'Aslan'}
+
+# c = {
+    
+#     'child1' : a,
+#     'child2' : b
+     
+#      }
+
+# print(c['child2']['ad'])
+
+
+
+# a = {1,2,3,4,5}
+
+# a.add(6)
+
+# print(a)
+
+
+# set1 = {"a", "b" , "c"}
+
+# set2 = {1, 2, 3}
+
+# set1.update(set2)
+
+# print(set1)
+
+# immutable
+
+# a = ( 1, 2, 3 )
+
+
+# print(type(a))
+
+# print(a.index(1))
+
+
+# tuple1 = (1,2,3,4,5)
+
+# tuple2 = (5,6,7,8,9)
+
+# tuple3 = tuple1 + tuple2
+
+# print(tuple3)
+
+
+
+
+
